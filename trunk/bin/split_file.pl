b9aa07074f74238ad82b408db73d70c352a28b38
gchen98@gmail.com <gchen98@gmail.com@7eb4e3fc-0c4b-6441-56c4-373da399108a>
2013-05-02 22:36:00 +0000

    reverted the version of split_kgp_file.sh to older version

--- trunk/bin/split_file.pl
+++ trunk/bin/split_file.pl
@@ -1,26 +1,26 @@
 #!/usr/bin/perl -w
 use strict;
 
-if (@ARGV<5){
-  print "<basefile> <partsize> <header=1|noheader=0> <chr> <sexfile>\n";
+if (@ARGV<4){
+  print "<basefile> <partsize> <header=1|noheader=0> <is_haploid>\n";
   exit(1);
 }
 
 my $basefile=shift;
 my $max=shift;
 my $is_header=shift;
-my $chr=shift;
-my $sexfile=shift;
+my $is_haploid=shift;
+#my $sexfile=shift;
 
 # read in the sexes
-my @sexarr=();
-open(IN,$sexfile)||die "Can't find $sexfile\n";
-<IN>;
-while(my $line=<IN>){
-  my ($seq,$sex)=split(/\t/,$line);
-  push(@sexarr,$sex);
-}
-close(IN);
+#my @sexarr=();
+#open(IN,$sexfile)||die "Can't find $sexfile\n";
+#<IN>;
+#while(my $line=<IN>){
+#  my ($seq,$sex)=split(/\t/,$line);
+#  push(@sexarr,$sex);
+#}
+#close(IN);
 
 my $i=0;
 my $c=0;
@@ -43,11 +43,13 @@ while(my $line=<STDIN>){
       print OUT3 "$header\n";
     }
   }
-  my $persons=scalar(@sexarr);
+  #my $persons=scalar(@sexarr);
+  my $persons=length($line);
   for(my $i=0;$i<$persons;++$i){
     my $c=substr($line,$i,1);
-    my $sex=$sexarr[$i];
-    if ($chr eq "X" && $sex eq "M" && $c eq "2"){
+    #my $sex=$sexarr[$i];
+    #if ($chr eq "X" && $sex eq "M" && $c eq "2"){
+    if ($is_haploid && $c eq "2"){
       $c="3";
     }
     print OUT3 "$c";
