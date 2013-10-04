4d3eccd10162885878d07ab49ac9fdb92e5d1830
gchen98@gmail.com <gchen98@gmail.com@7eb4e3fc-0c4b-6441-56c4-373da399108a>
2013-10-04 17:06:19 +0000

    A more GPU friendly version of denovo_reads penetrance function

--- branches/bam_support/src/penetrance/denovo_reads.cpp
+++ branches/bam_support/src/penetrance/denovo_reads.cpp
@@ -6,12 +6,58 @@
 #include"../clsafe.h"
 #endif
 
-float DenovoReadsMendelGPU::get_bam_loglikelihood(int len,float *  hap1prob,float *  hap2prob){
+
+void DenovoReadsMendelGPU::compute_penetrance(){
+  populate_read_matrices();
+  process_read_matrices();
+}
+
+void DenovoReadsMendelGPU::populate_read_matrices(){
+  memset(read_match_logmat,0.,sizeof(float)*g_people*read_full_matrix_size);
+  memset(read_mismatch_logmat,0.,sizeof(float)*g_people*read_full_matrix_size);
+  for(int subject=0;subject<g_people;++subject){
+    bool debug = subject==-50;
+    ReadParser & parser = parsers[subject];
+    if(debug) cerr<<"Extracting region for subject "<<subject<<" for left marker "<<g_left_marker<<" of markers length "<<g_markers<<"\n";
+    parser.extract_region(config->chromosome.data(),g_left_marker,g_markers);
+    if(debug) cerr<<"Printing data\n";
+    if(debug)parser.print_data();
+    // GPU friendly implementation
+    // get total depth
+    int expanded_row = 0;
+    for(uint matrow=0;matrow<parser.matrix_rows;++matrow){
+      int offset = parser.offset[matrow];
+      if(debug) cerr<<"Working on main read "<<matrow<<" with offset "<<offset<<endl;
+      for(uint j=0;j<g_max_window;++j){
+        read_alleles_mat[subject*read_compact_matrix_size+matrow*g_max_window+j] = parser.matrix_alleles[matrow*g_max_window+j];
+      }
+      // first get the depth of main read
+      int depth = parser.depth[matrow];
+      for(uint readindex=0;readindex<depth;++readindex){
+        superread_indices[subject*full_rows+expanded_row] = matrow;
+        for(uint baseindex=0;baseindex<parser.read_len[matrow];
+        ++baseindex){
+          // APPROACH 1: GPU friendly
+          int phredscore = parser.base_quality[matrow][readindex][baseindex];
+          //if(debug) cerr<<"Phred score is "<<phredscore<<endl;
+          read_match_logmat[subject*read_full_matrix_size+expanded_row*g_max_window+offset+baseindex] = parser.logprob_match_lookup[phredscore];
+          read_mismatch_logmat[subject*read_full_matrix_size+expanded_row*g_max_window+offset+baseindex] = parser.logprob_mismatch_lookup[phredscore];
+          if (debug)cerr<<"POPULATE for row "<<expanded_row<<" col "<<offset+baseindex<<" match,mismatch: "<<read_match_logmat[subject*read_full_matrix_size+expanded_row*g_max_window+offset+baseindex]<<","<<read_mismatch_logmat[subject*read_full_matrix_size+expanded_row*g_max_window+offset+baseindex]<<endl;
+        }
+        ++expanded_row;
+      }   
+    } 
+    mat_rows_by_subject[subject] = expanded_row;
+  }
+  return;
+}
+
+float DenovoReadsMendelGPU::get_bam_loglikelihood(int len,float *  hap1logprob,float *  hap2logprob){
   float logh1 = 0,logh2 = 0;
   for(int i=0;i<len;++i){
-    //cerr<<"debug"<<i<<":"<<hap1prob[i]<<","<<hap2prob[i]<<endl;
-    logh1+=hap1prob[i]!=1?log(hap1prob[i]):0;
-    logh2+=hap2prob[i]!=1?log(hap2prob[i]):0;
+    //cerr<<"debug"<<i<<":"<<hap1logprob[i]<<","<<hap2logprob[i]<<endl;
+    logh1+=hap1logprob[i]!=0?(hap1logprob[i]):0;
+    logh2+=hap2logprob[i]!=0?(hap2logprob[i]):0;
   }
   float mean = (logh1+logh2)/2.;
   //cerr<<"Mean offset is "<<mean<<endl;
@@ -19,100 +65,95 @@ float DenovoReadsMendelGPU::get_bam_loglikelihood(int len,float *  hap1prob,floa
   return loglike;
 }
 
-void DenovoReadsMendelGPU::compute_penetrance(){
+void DenovoReadsMendelGPU::process_read_matrices(){
   for(int subject=0;subject<g_people;++subject){
+    bool debug = subject==-50;
     float max_log_pen = -1e10;
     for(int hap1=0;hap1<g_max_haplotypes;++hap1){
       for(int hap2=hap1;hap2<g_max_haplotypes;++hap2){
         if (g_active_haplotype[hap1] && g_active_haplotype[hap2]){
-          cerr<<"Computing reads penetrance for subject "<<subject<<" haps "<<hap1<<","<<hap2<<endl;
-          ReadParser & parser = parsers[subject];
-          cerr<<"Extracting region for left marker "<<g_left_marker<<" of markers length "<<g_markers<<"\n";
-          parser.extract_region(config->chromosome.data(),g_left_marker,g_markers);
-          //cerr<<"Printing data\n";
-          //parser.print_data();
+          if(debug) cerr<<"Computing reads penetrance for subject "<<subject<<" haps "<<hap1<<","<<hap2<<endl;
+          if(debug) cerr<<hap1<<":";
+          for(int i=0;i<g_markers;++i) if(debug) cerr<<g_haplotype[hap1*g_max_window+i];
+          if(debug) cerr<<endl;
+          if(debug) cerr<<hap2<<":";
+          for(int i=0;i<g_markers;++i) if(debug) cerr<<g_haplotype[hap2*g_max_window+i];
+          if(debug) cerr<<endl;
           // GPU friendly implementation
           // get total depth
-          int total_depth = 0;
-          int total_width = parser.MATRIX_WIDTH;
-          for(uint matrow=0;matrow<parser.matrix_rows;++matrow){
-            total_depth += parser.depth[matrow];
-          }
-          float prob_matrix1[total_depth*total_width];
-          float prob_matrix2[total_depth*total_width];
-          for(int i=0;i<total_depth*total_width;++i){
-            prob_matrix1[i] = prob_matrix2[i] = 1;
-          }
-          int expanded_row = 0;
-          
-        
-          // original implementation 
-          float loglike = 0;
-  
-          for(uint matrow=0;matrow<parser.matrix_rows;++matrow){
-            int offset = parser.offset[matrow];
-            cerr<<"Working on main read "<<matrow<<" with offset "<<offset<<endl;
-            // first get the depth of main read
-            int depth = parser.depth[matrow];
-            for(uint readindex=0;readindex<depth;++readindex){
-              //float hap1_prob[parser.read_len[matrow]];
-              //float hap2_prob[parser.read_len[matrow]];
-              //float hap1_like=1,hap2_like=1;
-              for(uint baseindex=0;baseindex<parser.read_len[matrow];
-              ++baseindex){
-                // APPROACH 1: GPU friendly
-                prob_matrix1[expanded_row*total_width+offset+baseindex] =g_haplotype[hap1*g_max_window+baseindex]==
-                parser.matrix_alleles[matrow*parser.MATRIX_WIDTH+baseindex]? 
-                (parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex])):
-                (1.-parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex]));
-                prob_matrix2[expanded_row*total_width+offset+baseindex] =g_haplotype[hap2*g_max_window+baseindex]==
-                parser.matrix_alleles[matrow*parser.MATRIX_WIDTH+baseindex]? 
-                (parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex])):
-                (1.-parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex]));
-
-                // APPROACH 2: will not underflow
-                //hap1_prob[baseindex] =g_haplotype[hap1*g_max_window+baseindex]==
-                //parser.matrix_alleles[matrow*parser.MATRIX_WIDTH+baseindex]? 
-                //(parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex])):
-                //(1.-parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex]));
-                //hap2_prob[baseindex] =g_haplotype[hap2*g_max_window+baseindex]==
-                //parser.matrix_alleles[matrow*parser.MATRIX_WIDTH+baseindex]? 
-                //(parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex])):
-                //(1.-parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex]));
-
-                // APPROACH 3: this approach below may underflow with longer haplotypes 
-                //hap1_like*=g_haplotype[hap1*g_max_window+baseindex]==
-                //parser.matrix_alleles[matrow*parser.MATRIX_WIDTH+baseindex]?
-                //(parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex])):
-                //(1.-parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex]));
-                //hap2_like*=g_haplotype[hap2*g_max_window+baseindex]==
-                //parser.matrix_alleles[matrow*parser.MATRIX_WIDTH+baseindex]?
-                //(parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex])):
-                //(1.-parser.phred2prob(parser.base_quality[matrow][readindex*parser.read_len[matrow]+offset+baseindex]));
-              }
-              //float like1 = log(.5*(hap1_like+hap2_like)); 
-              //float like2 = get_bam_loglikelihood(parser.read_len[matrow],hap1_prob,hap2_prob);
-              //cerr<<"For sub read "<<readindex<<" hap_probs are "<<hap1_like<<" and "<<hap2_like<<" and logprobs are "<<like1<<" and "<<like2<<endl;
-              //loglike+=like2;
+          int total_depth = mat_rows_by_subject[subject];
+          int total_width = g_max_window;
+          //explore the entire matrix 
+          float log_like = 0;
+          for(int row=0;row<total_depth;++row){
+            float hap1_logprob[g_max_window];
+            float hap2_logprob[g_max_window];
+            int superread_index = superread_indices[subject*full_rows+row];
+            for(int col=0;col<total_width;++col){
+              int allele = read_alleles_mat[subject*read_compact_matrix_size+superread_index*g_max_window+col] ;
+              if(allele!=9 && col<g_markers){
+                hap1_logprob[col] = g_haplotype[hap1*g_max_window+col]==allele?
+                read_match_logmat[subject*read_full_matrix_size+row*g_max_window+col]:
+                read_mismatch_logmat[subject*read_full_matrix_size+row*g_max_window+col];
+                hap2_logprob[col] = g_haplotype[hap2*g_max_window+col]==allele?
+                read_match_logmat[subject*read_full_matrix_size+row*g_max_window+col]:
+                read_mismatch_logmat[subject*read_full_matrix_size+row*g_max_window+col];
+                if (debug)cerr<<"for row,col "<<row<<","<<col<<" GOT HERE with vals "<<hap1_logprob[col]<<","<<hap2_logprob[col]<<"\n";
              
-              
-              //cout<<matrix_alleles[i*MATRIX_WIDTH+j];
-              ++expanded_row;
-            }   
-          } 
-          float loglike2 = 0;
-          for(expanded_row=0;expanded_row<total_depth;++expanded_row){
-            loglike2+=get_bam_loglikelihood(total_width,
-            prob_matrix1+expanded_row*total_width,
-            prob_matrix2+expanded_row*total_width);
+              }else{
+                hap1_logprob[col] = 0;
+                hap2_logprob[col] = 0;
+              }
+            }
+            float ll = get_bam_loglikelihood(g_max_window,hap1_logprob,hap2_logprob);
+            //if (debug) cerr<<"current log like "<<ll<<endl; 
+            log_like+=ll;
           }
-          cerr<<"Log likelihood of all reads for haps "<<hap1<<","<<hap2<<" is "<<loglike2<<endl;
-          if (loglike2>max_log_pen) max_log_pen = loglike2;
-          logpenetrance_cache[subject*penetrance_matrix_size+hap1*g_max_haplotypes+hap2] = loglike2;
+
+
+//          float logprob_matrix1[total_depth*total_width];
+//          float logprob_matrix2[total_depth*total_width];
+//          for(int i=0;i<total_depth*total_width;++i){
+//            logprob_matrix1[i] = logprob_matrix2[i] = 0;
+//          }
+//          int expanded_row = 0;
+//          for(uint matrow=0;matrow<parser.matrix_rows;++matrow){
+//            int offset = parser.offset[matrow];
+//            if(debug) cerr<<"Working on main read "<<matrow<<" with offset "<<offset<<endl;
+//            // first get the depth of main read
+//            int depth = parser.depth[matrow];
+//            for(uint readindex=0;readindex<depth;++readindex){
+//              for(uint baseindex=0;baseindex<parser.read_len[matrow];
+//              ++baseindex){
+//                // APPROACH 1: GPU friendly
+//                int phredscore = parser.base_quality[matrow][readindex][baseindex];
+//                //if(debug) cerr<<"Phred score is "<<phredscore<<endl;
+//                logprob_matrix1[expanded_row*total_width+baseindex]=g_haplotype[hap1*g_max_window+offset+baseindex]==
+//                parser.matrix_alleles[matrow*parser.max_matrix_width+offset+baseindex]? 
+//                (parser.logprob_match_lookup[phredscore]):
+//                (parser.logprob_mismatch_lookup[phredscore]);
+//                logprob_matrix2[expanded_row*total_width+baseindex] =g_haplotype[hap2*g_max_window+offset+baseindex]==
+//                parser.matrix_alleles[matrow*parser.max_matrix_width+offset+baseindex]? 
+//                (parser.logprob_match_lookup[phredscore]):
+//                (parser.logprob_mismatch_lookup[phredscore]);
+//
+//              }
+//              ++expanded_row;
+//            }   
+//          } 
+//          float log_like = 0;
+//          for(expanded_row=0;expanded_row<total_depth;++expanded_row){
+//            log_like+=get_bam_loglikelihood(total_width,
+//            logprob_matrix1+expanded_row*total_width,
+//            logprob_matrix2+expanded_row*total_width);
+//          }
+          if(debug) cerr<<"Log likelihood of all reads for haps "<<hap1<<","<<hap2<<" is "<<log_like<<endl;
+          if (log_like>max_log_pen) max_log_pen = log_like;
+          logpenetrance_cache[subject*penetrance_matrix_size+hap1*g_max_haplotypes+hap2] = log_like;
         } // active hap condition
       } // second hap loop
     }// first hap loop
-    //cerr<<"Max log penetrance for subject "<<subject<<" is "<<max_log_pen<<endl;
+    //if(debug) cerr<<"Max log penetrance for subject "<<subject<<" is "<<max_log_pen<<endl;
     for(int hap1=0;hap1<g_max_haplotypes;++hap1){
       for(int hap2=hap1;hap2<g_max_haplotypes;++hap2){
         if (g_active_haplotype[hap1] && g_active_haplotype[hap2]){
@@ -127,19 +168,13 @@ void DenovoReadsMendelGPU::compute_penetrance(){
           penetrance_cache[subject*penetrance_matrix_size+hap2*
           g_max_haplotypes+hap1] = penetrance_cache[subject*
           penetrance_matrix_size+hap1*g_max_haplotypes+hap2];
-          //cerr<<"Penetrance for subject "<<subject<<" haps "<<hap1<<","<<hap2<<": "<<penetrance_cache[subject*penetrance_matrix_size+hap1*g_max_haplotypes+hap2]<<endl;
+          //if(debug) cerr<<"Penetrance for subject "<<subject<<" haps "<<hap1<<","<<hap2<<": "<<penetrance_cache[subject*penetrance_matrix_size+hap1*g_max_haplotypes+hap2]<<endl;
         } // active hap condition
       } // second hap loop
     }// first hap loop
   }
-  return;
-
-  // if we are in sequencing mode, assume non polymorphic sites are ref allele 
-  if (g_likelihood_mode == Config::LIKELIHOOD_MODE_READS && !polymorphic_window){
-    cerr<<"Skipping non polymorphic site\n";
-    return;
-  }
   cerr<<"Exiting compute penetrance\n";
+  return;
 }
 
 
--- branches/bam_support/src/read_penetrance.hpp
+++ branches/bam_support/src/read_penetrance.hpp
@@ -33,22 +33,29 @@ public:
   static void parse_positions(const char * filename);
   void extract_region(const char * chrom,int snp_offset,int snps);
   void print_data();
-  static float phred2prob(int phred);
+  
+  static void init_phred_lookup(int max_width);
   // utility function
-  //static inline float phred2prob(int phred){
-   // return 1-(pow(10,(-phred/10)));
-  //}
+  static inline float phred2prob1(int phred){
+    return 1-(pow(10,(-phred/10)));
+  }
   vector<int> found_positions;
+  static uint max_matrix_width;
+  static uint max_matrix_height;
+  static const uint MAX_SUPERREADS = 10;
+  static const uint MAX_SUBREADS = 50;
+  static const uint MAX_READ_LEN = 10;
   static const float EPSILON=1e-2;
-  static const uint MATRIX_WIDTH=20;
-  static const uint MATRIX_HEIGHT=200;
+  static float min_log_phred,max_log_phred;
+  static float logprob_match_lookup[];
+  static float logprob_mismatch_lookup[];
   uint matrix_rows;
   int * matrix_alleles;
   int * offset;
   int * depth;
   int * read_len;
   int ** map_quality;
-  int ** base_quality;
+  int *** base_quality;// dimensions are superreads, subreads, and bases
 private:
   static bool debug; 
   bam_index_t *idx;
